ae8dc2a1cf6ba53bd6b3e6d3d5d81468feb28257
Pavel Orel <pavel.d.orel@gmail.com>
2022-11-13 16:33:46 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,12 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.7
 import PackageDescription
-
-extension BuildItem {
-  static let opFoundation: BuildItem = .init(name: "OPFoundation")
-  static let opCombine: BuildItem = .core(name: "OPCombine")
-
-  static let opTypography: BuildItem = .core(name: "OPTypography")
-  static let opSwiftUI: BuildItem = .core(name: "OPSwiftUI")
-  static let opUIKit: BuildItem = .core(name: "OPUIKit")
-}
-
 
 // MARK: - Package
 
 let items: [BuildItem] = [
-  .opFoundation, .opCombine,
-  .opTypography, .opSwiftUI,
-  .opUIKit
+  .init(name: "OPFoundation"), .init(name: "OPCombine"),
+  .init(name: "OPTypography"), .init(name: "OPSwiftUI"),
+  .init(name: "OPUIKit")
 ]
 
 let package = Package(
@@ -51,9 +41,5 @@ struct BuildItem {
   
   init(name: String, dependencies: [BuildItem], resources: [Resource] = []) {
     self.init(name: name, dependency: nil, dependencies: dependencies.map(\.dependency), resources: resources)
-  }
-  
-  static func core(name: String, adding extras: [BuildItem] = [], resources: [Resource] = []) -> BuildItem {
-    .init(name: name, dependencies: [.opFoundation] + extras, resources: resources)
   }
 }
