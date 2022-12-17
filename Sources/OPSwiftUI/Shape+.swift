253c2942ee98b6eb4ded7f604948a0ec4e57fbc5
Pavel Orel <pavel.d.orel@gmail.com>
2022-12-16 23:49:16 -0500

    Add fill + stroke Shape helper

---
+++ Sources/OPSwiftUI/Shape+.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+public extension Shape {
+  func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(
+    _ fillStyle: Fill,
+    stroke strokeStyle: Stroke,
+    lineWidth: Double = 1) -> some View {
+      self
+        .stroke(strokeStyle, lineWidth: lineWidth)
+        .background(self.fill(fillStyle))
+    }
+}
+
+public extension InsettableShape {
+  func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(
+    _ fillStyle: Fill,
+    strokeBorder strokeStyle: Stroke,
+    lineWidth: Double = 1) -> some View {
+      self
+        .strokeBorder(strokeStyle, lineWidth: lineWidth)
+        .background(self.fill(fillStyle))
+    }
+}
